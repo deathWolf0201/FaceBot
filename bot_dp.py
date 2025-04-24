13080afdcd57336c7901b5b5c96092a0870a2ae2
deathWolf0201 <128875479+deathWolf0201@users.noreply.github.com>
2025-04-24 17:11:44 +1000

    Adding deleteWebhook

--- bot_dp.py
+++ bot_dp.py
@@ -2,7 +2,11 @@
 
 from aiogram import Dispatcher, Bot
 from config import TOKEN
+import requests
 
+url = f"https://api.telegram.org/bot{TOKEN}/deleteWebhook"
+response = requests.get(url)
+print(response.json())  # Должно быть {"ok": True, "result": True}
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
